faa910125a7c08ed8c8e7f67646c73dfd6e07961
yumayamada <you.yamada.20015@hotmail.com>
2016-01-07 22:13:33 +0900

    カラムの追加、バリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:name, :part_1,:user_id,:Birth_date,:gender,:start_at_part_1,:start_at_part_2,:current_place,:start_at_band]
+      devise_parameter_sanitizer.for(:sign_in) <<[:name, :part_1,:user_id,:Birth_date,:gender,:start_at_part_1,:start_at_part_2,:current_place,:start_at_band]
+      devise_parameter_sanitizer.for(:account_update) << [:name, :part_1,:user_id,:Birth_date,:gender,:start_at_part_1,:start_at_part_2,:current_place,:start_at_band]
+    end
+  
 end
