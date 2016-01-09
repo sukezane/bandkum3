9e291f010f9d2fcf58f199e9418ac52966d3bc6c
yumayamada <you.yamada.20015@hotmail.com>
2016-01-09 11:53:32 +0900

    changed serve-side

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,6 +2,7 @@ require 'pusher'
 
 Pusher.url = "https://7cadebb65622e49e0891:d364bc96b97a34a5d397@api.pusherapp.com/apps/165332"
 
-Pusher.trigger('test_channel', 'my_event', {
-  message: 'hello world'
-})
+# Pusher.trigger('test_channel', 'my_event', {
+#   message: 'hello world'
+# })
+Pusher.logger = Rails.logger
