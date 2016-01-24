40cfea4928bb311f4d880cb5f2ea451c3338dca1
yumayamada <you.yamada.20015@hotmail.com>
2016-01-24 17:45:06 +0900

    検索時の修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-     @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
+     # @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
       # @conversations = Conversation.involving(current_user).order("created_at DESC")
       @q = User.ransack(params[:q])
      @users = @q.result(distinct: true)
